c1a8e5e3bb03d92b17662b2b19430da927eaf450
Nirob Hasan <thenirobhasan@gamil.com>
2021-11-29 23:48:15 +0600

    blogs

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const { MongoClient } = require('mongodb');
 const ObjectID = require('mongodb').ObjectId;
 require('dotenv').config();
 
+// npm i express-fileupload
+const fileUpload = require('express-fileupload')
 const app= express();
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload())
+
+
 const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jiiff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,6 +24,7 @@ async function run() {
       const tourPackageCollection = database.collection("tour-package");
       const placedOrderCollecttion = database.collection("placeOrder");
       const reviewsCollection = database.collection('reviews')
+      const blogsCollection = database.collection('blogs')
 
       //Get all package
       app.get("/services", async(req,res)=>{
@@ -105,6 +111,42 @@ async function run() {
         res.json(reviews)
 
       })
+
+      // blog section 
+
+      app.post('/blog', async(req,res)=>{
+        const title = req.body.title;
+        const blogBody = req.body.blog;
+        const writerName = req.body.blog_writer;
+        const img = req.files.image;
+        const imgData = img.data;
+        const encodedImg = imgData.toString('base64');
+        const imageBuffer = Buffer.from(encodedImg, 'base64')
+
+        const blog = {
+          writerName,
+          title,
+          blogBody,
+          image: imageBuffer
+        }
+        const result = await blogsCollection.insertOne(blog);
+        res.json(result);
+      })
+
+     //Get blogs
+     app.get('/blogs', async(req,res)=>{
+       const cursor = blogsCollection.find({});
+       const blogs = await cursor.toArray();
+       res.json(blogs)
+     })
+     //Get Single Blog
+     app.get('/blogs/:id',async(req,res)=>{
+       const id = req.params.id;
+       const query = {_id: ObjectID(id)}
+       const blog = await blogsCollection.findOne(query)
+       res.json(blog);
+     }) 
+
       
     } finally {
     //   await client.close();
